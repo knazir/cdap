343b62a2a42497dea36f428d6714b047e9372097
Terence Yim <terence@cask.co>
2016-11-22 12:32:22 -0800

    (CDAP-7648) Added a config for the ordering of hive vs yarn classpath
    - Also standardize on how we trim the classpath setting

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -20,7 +20,7 @@ source ../vars
 source ../_common/common-build.sh
 
 DEFAULT_XML="../../cdap-common/src/main/resources/cdap-default.xml"
-DEFAULT_XML_MD5_HASH="9e190beadf969e7edd870d339f129c06"
+DEFAULT_XML_MD5_HASH="f258998c4189fd390978e7249e8cab87"
 
 DEFAULT_TOOL="../tools/cdap-default/doc-cdap-default.py"
 DEFAULT_DEPRECATED_XML="../tools/cdap-default/cdap-default-deprecated.xml"
